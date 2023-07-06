5bcdf6b0f7e0081b24315aa26939e90e5059f6c5
rkbennett <44292326+rkbennett@users.noreply.github.com>
2023-07-06 10:43:59 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// index.js
+//const scrape = import('website-scraper');
+//const PuppeteerPlugin = import('website-scraper-puppeteer');
+import scrape from 'website-scraper';
+import PuppeteerPlugin from 'website-scraper-puppeteer';
+//const path = require('path');
+
+function endsWithAny(suffixes, string) {
+    return suffixes.some(function (suffix) {
+        return string.endsWith(suffix);
+    });
+}
+
+console.log("Scraping: " + process.env.PROTO + '://' + process.env.URL + '/');
+console.log("Depth: " + process.env.DEPTH);
+scrape({
+    // Provide the URL(s) of the website(s) that you want to clone
+    // In this example, you can clone the Our Code World website
+    //urls: ['https://www.apple.com/'],
+    urls: [process.env.PROTO + '://' + process.env.URL + '/'],
+    urlFilter: (url) => !(endsWithAny(['.mp4', '.mp3', '.mpeg'], url)),
+    // Specify the path where the content should be saved
+    // In this case, in the current directory inside the ourcodeworld dir
+    //directory: '/tmp/www.apple.com',
+    directory: '/scrape/' + process.env.URL,
+    maxRecursiveDepth: process.env.DEPTH,
+    recursive: Boolean(parseInt(process.env.DEPTH)),
+    requestConcurrency: 30,
+    // Load the Puppeteer plugin
+    plugins: [ 
+        new PuppeteerPlugin({
+            launchOptions: { headless: true }, /* optional */
+            // launchOptions: { headless: true, args: ["--no-sandbox","--no-zygote"] }, /* optional */
+            scrollToBottom: { 
+		timeout: 10000,
+                viewportN: 10
+            }, /* optional */
+            blockNavigation: false,
+	    evaluatePage: async(page) => {
+                const iframes = await page.$$('iframe');
+                for (const iframe of iframes) {
+		    console.log(iframe);
+                    const iframeWidth = await iframe.getProperty('width').jsonValue();
+                    const iframeHeight = await iframe.getProperty('height').jsonValue();
+                    const iframeVisibility = await iframe.getProperty('style').then(style => style.jsonValue().visibility);
+		    console.log(iframeHeight);
+		    console.log(iframeWidth);
+                    if ((iframeWidth === 0 && iframeHeight === 0) || iframeVisibility === 'hidden') {
+                        await iframe.evaluate((iframe) => iframe.remove());
+	            }
+		}
+	    }
+        })
+    ]
+});
+
---
+++ website-scraper-puppeteer.js
@@ -0,0 +1,160 @@
+import puppeteer from 'puppeteer';
+import logger from './logger.js';
+import scrollToBottomBrowser from './browserUtils/scrollToBottom.js';
+
+class PuppeteerPlugin {
+	constructor ({
+		launchOptions = {},
+		scrollToBottom = null,
+		blockNavigation = false,
+		blockAdSense = true
+	} = {}) {
+		this.launchOptions = launchOptions;
+		this.scrollToBottom = scrollToBottom;
+		this.blockNavigation = blockNavigation;
+		this.browser = null;
+		this.headers = {};
+
+		logger.info('init plugin', { launchOptions, scrollToBottom, blockNavigation, blockAdSense });
+	}
+
+	apply (registerAction) {
+		registerAction('beforeStart', async () => {
+			this.browser = await puppeteer.launch(this.launchOptions);
+		});
+
+		registerAction('beforeRequest', async ({requestOptions}) => {
+			if (hasValues(requestOptions.headers)) {
+				this.headers = Object.assign({}, requestOptions.headers);
+			}
+			return {requestOptions};
+		});
+
+		registerAction('afterResponse', async ({response}) => {
+			const contentType = response.headers['content-type'];
+			const isHtml = contentType && contentType.split(';')[0] === 'text/html';
+			if (isHtml) {
+				const url = response.url;
+				const page = await this.browser.newPage();
+
+				if (hasValues(this.headers)) {
+					logger.info('set headers to puppeteer page', this.headers);
+					await page.setExtraHTTPHeaders(this.headers);
+				}
+
+				if (this.blockNavigation) {
+					await blockNavigation(page, url);
+				}
+
+				await page.goto(
+				    url,
+				    { waitUntil: "load",
+				});
+
+				if (this.scrollToBottom) {
+					await scrollToBottom(page, this.scrollToBottom.timeout, this.scrollToBottom.viewportN);
+				}
+
+				await page.evaluate(() => {
+				    var cookieBtn = document.querySelector('#onetrust-accept-btn-handler');
+				    if(cookieBtn){
+					cookieBtn.click();
+				    }
+				});
+
+				if (blockAdSense){
+				    const iframes = await page.$$('iframe');
+			            for (const iframe of iframes) {
+					const frameBox = await iframe.boundingBox();
+					const frameVisibility = await iframe.evaluate(frame => window.getComputedStyle(frame).getPropertyValue('visibility'));
+					const frameDisplay = await iframe.evaluate(frame => window.getComputedStyle(frame).getPropertyValue('display'));
+				        const iframeWidthPromise = await iframe.getProperty('width');
+				        const iframeHeightPromise = await iframe.getProperty('height');
+					var iframeWidth = await iframeWidthPromise.jsonValue();
+					var iframeHeight = await iframeWidthPromise.jsonValue();
+					//console.log(frameVisibility);
+					//console.log(frameVisibility === 'hidden');
+					//console.log(frameDisplay);
+					//console.log(frameDisplay === 'none');
+					//const frameContent = await iframe.contentFrame()
+					//console.log(frameContent);
+					if (iframeHeight == 0 || iframeWidth == 0 || frameVisibility === 'hidden' || frameDisplay === 'none') {
+					    //console.log(frameContent._url);
+				            await page.evaluate((frame) => frame.remove(), iframe);
+					};
+					//console.log(frameContent._url.includes("doubleclick.net"));
+					//if (frameContent._url.includes("doubleclick.net") || frameContent._url.includes("sync.getpublica.com")) {
+					    //console.log(iframeHeight == 0);
+					    //console.log(iframeWidth);
+					    //console.log(frameContent._url);
+				            //await page.evaluate((frame) => frame.remove(), iframe);
+					//}
+			            };
+				};
+
+				const content = await page.content();
+				content.includes("src=\"getuid")
+				await page.close();
+
+				// convert utf-8 -> binary string because website-scraper needs binary
+				return Buffer.from(content).toString('binary');
+			} else {
+				return response.body;
+			}
+		});
+
+		registerAction('afterFinish', () => this.browser && this.browser.close());
+	}
+}
+
+function hasValues (obj) {
+	return obj && Object.keys(obj).length > 0;
+}
+
+
+async function scrollToBottom (page, timeout, viewportN) {
+	logger.info(`scroll puppeteer page to bottom ${viewportN} times with timeout = ${timeout}`);
+
+	await page.evaluate(scrollToBottomBrowser, timeout, viewportN);
+}
+
+async function blockNavigation (page, url) {
+	logger.info(`block navigation for puppeteer page from url ${url}`);
+
+	page.on('request', req => {
+		if (req.isNavigationRequest() && req.frame() === page.mainFrame() && req.url() !== url) {
+			req.abort('aborted');
+		} else {
+			req.continue();
+		}
+	});
+	await page.setRequestInterception(true);
+}
+
+async function blockAdSense (page) {
+	logger.info(`drop iframes with size of 0x0 or style of hidden (typically used for tracking iframes)`);
+	const pageContent = page;
+	const iframes = await page.$$('iframe');
+	//await page.evaluate((pageContent) => {
+	//	const test = pageContent.$$('iframe');
+	//});
+	/*await page.evaluate({
+	    const iframes = await page.$$('iframe');
+            for (const iframe of iframes) {
+                console.log(iframe);
+                const iframeWidth = await iframe.getProperty('width').jsonValue();
+                const iframeHeight = await iframe.getProperty('height').jsonValue();
+                const iframeVisibility = await iframe.getProperty('style').then(style => style.jsonValue().visibility);
+                console.log(iframeHeight);
+                console.log(iframeWidth);
+                if ((iframeWidth === 0 && iframeHeight === 0) || iframeVisibility === 'hidden') {
+                    await iframe.evaluate((iframe) => iframe.remove());
+                };
+            };
+	});*/
+}
+
+
+
+
+export default PuppeteerPlugin;
