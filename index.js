e88a52c4129f1a2d7f99463eb3561ea0eb8314bd
rkbennett <44292326+rkbennett@users.noreply.github.com>
2023-07-18 08:24:44 -0500

    fixes syntax error

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ scrape({
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
       }
-    }
+    },
     // Load the Puppeteer plugin
     plugins: [ 
         new PuppeteerPlugin({
