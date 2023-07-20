3921ccc3c140ad1e717efeacc1ca06bbd737b1f7
rkbennett <44292326+rkbennett@users.noreply.github.com>
2023-07-20 10:38:34 -0400

    updates to new headless mode

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ function endsWithAny(suffixes, string) {
 console.log("Scraping: " + process.env.PROTO + '://' + process.env.URL + '/');
 console.log("Depth: " + process.env.DEPTH);
 scrape({
-    // Provide the URL(s) of the website(s) that you want to clone
-    // In this example, you can clone the Our Code World website
-    //urls: ['https://www.apple.com/'],
-    urls: [process.env.PROTO + '://' + process.env.URL + '/'],
-    urlFilter: (url) => !(endsWithAny(['.mp4', '.mp3', '.mpeg'], url)),
-    // Specify the path where the content should be saved
-    // In this case, in the current directory inside the ourcodeworld dir
-    //directory: '/tmp/www.apple.com',
-    directory: '/scrape/' + process.env.URL,
+    // Provide the URL(s) of the website(s) that you want to clone                                                                                                                                                                         
+    // In this example, you can clone the Our Code World website                                                                                                                                                                           
+    //urls: ['https://www.apple.com/'],                                                                                                                                                                                                    
+    urls: [process.env.PROTO + '://' + process.env.URL + '/'],                                                                                                                                                                             
+    urlFilter: (url) => !(endsWithAny(['.mp4', '.mp3', '.mpeg'], url)),                                                                                                                                                                    
+    // Specify the path where the content should be saved                                                                                                                                                                                  
+    // In this case, in the current directory inside the ourcodeworld dir                                                                                                                                                                  
+    //directory: '/tmp/www.apple.com',                                                                                                                                                                                                     
+    directory: '/scrape/' + process.env.URL,                                                                                                                                                                                               
     maxRecursiveDepth: process.env.DEPTH,
     recursive: Boolean(parseInt(process.env.DEPTH)),
     requestConcurrency: 30,
@@ -34,28 +34,30 @@ scrape({
     // Load the Puppeteer plugin
     plugins: [ 
         new PuppeteerPlugin({
-            launchOptions: { headless: true }, /* optional */
+            launchOptions: { 
+                headless: "new",
+                executablePath: "/usr/bin/chromium"
+            }, /* optional */
             // launchOptions: { headless: true, args: ["--no-sandbox","--no-zygote"] }, /* optional */
             scrollToBottom: { 
-		timeout: 10000,
+                timeout: 10000,
                 viewportN: 10
             }, /* optional */
             blockNavigation: true,
-	    evaluatePage: async(page) => {
+            evaluatePage: async(page) => {
                 const iframes = await page.$$('iframe');
                 for (const iframe of iframes) {
-		    console.log(iframe);
+                    console.log(iframe);
                     const iframeWidth = await iframe.getProperty('width').jsonValue();
                     const iframeHeight = await iframe.getProperty('height').jsonValue();
                     const iframeVisibility = await iframe.getProperty('style').then(style => style.jsonValue().visibility);
-		    console.log(iframeHeight);
-		    console.log(iframeWidth);
+                    console.log(iframeHeight);
+                    console.log(iframeWidth);
                     if ((iframeWidth === 0 && iframeHeight === 0) || iframeVisibility === 'hidden') {
                         await iframe.evaluate((iframe) => iframe.remove());
-	            }
-		}
-	    }
+                    }
+                }
+            }
         })
     ]
 });
-
