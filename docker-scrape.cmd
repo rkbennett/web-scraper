d40dd7a1bd03dd3e84eeab32809324089a6defd3
rkbennett <44292326+rkbennett@users.noreply.github.com>
2023-07-20 10:41:06 -0400

    Update docker-scrape.cmd

--- docker-scrape.cmd
+++ docker-scrape.cmd
@@ -1,1 +1,1 @@
-docker run -dit --cap-add=SYS_ADMIN --name testing -e DEPTH=3 -e URL=www.apple.com -e PROTO=https --rm -v /tmp:/scrape scraper
+docker run -dit --cap-add=SYS_ADMIN --name testing -e DEPTH=0 -e URL=www.apple.com -e PROTO=https --rm -v /tmp:/scrape scraper
